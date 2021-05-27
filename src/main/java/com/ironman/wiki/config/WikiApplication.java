ed048b0ccc728d82218d4b74b7ff41c19ef16c0f
ironman327 <fangcongrui@qq.com>
2021-05-27 21:44:14 +0800

    2-7 开发hello world接口

--- src/main/java/com/ironman/wiki/config/WikiApplication.java
+++ src/main/java/com/ironman/wiki/config/WikiApplication.java
@@ -1,14 +1,15 @@
-package com.ironman.wiki;
+package com.ironman.wiki.config;
 
 
-import com.oracle.tools.packager.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 
+@ComponentScan("com.ironman")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
