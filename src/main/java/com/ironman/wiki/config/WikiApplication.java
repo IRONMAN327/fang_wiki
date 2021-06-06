25ceecae68c8916364f9c6b84c80d80b305921b3
ironman327 <fangcongrui@qq.com>
2021-06-06 15:24:09 +0800

    3-1 集成mybatis

--- src/main/java/com/ironman/wiki/config/WikiApplication.java
+++ src/main/java/com/ironman/wiki/config/WikiApplication.java
@@ -1,6 +1,7 @@
 package com.ironman.wiki.config;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,7 @@ import org.springframework.core.env.Environment;
 
 @ComponentScan("com.ironman")
 @SpringBootApplication
+@MapperScan("com.ironman.wiki.mapper")
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
